fd31a6a834fa48009b58f7631bf2b98fd98b6a50
Rezahasyim <75386161+Rezahasyim@users.noreply.github.com>
2020-12-14 21:58:53 +0700

    Create python2.py

---
+++ python2.py
@@ -0,0 +1,156 @@
+#!bin/user/python
+
+#decompiled by PDM31
+
+import os, sys
+
+print '\x1b[1;32mMasukkan Pasword Dulu >
+
+print '\x1b[1;32mSilahkan Login '
+
+import os, sys
+
+def wa():
+
+    os.system('xdg-open https://api.wha>
+
+def restart():
+
+    ngulang = sys.executable
+
+    os.execl(ngulang, ngulang, *sys.arg>
+
+user = raw_input('ID: ')
+
+import getpass
+
+sandi = raw_input('Password: ')
+
+if sandi == 'bahaya' and user == 'virus>
+
+    print 'Anda Telah Login'
+
+    sys.exit
+
+else:
+
+    print 'Login GAGAL, Silahkan hubung>
+
+    wa()
+
+    restart()
+
+import os, sys, time, datetime, random,>
+
+from multiprocessing.pool import Thread>
+
+try:
+import requests
+
+    except ImportError:
+
+        os.system('pip2 install request>
+
+time.sleep(10)
+
+print "Loe Telah login hp lo dalam baha>
+
+print "hati-hati hp anda sedang di reta>
+
+print "segera Reset hp anda"
+
+print
+
+print
+
+print
+
+print
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+
+time.sleep(0.5)
+
+print "Lo Anjing"
+print "selamat anda kena prank Kameranya aku taroh di situ"
